cdd24f7dda14ed4addd9e9d8b6bac0d6a835fa51
miracline2 <francesca4518@gmail.com>
2025-03-20 21:23:33 +0530

    Updation in getRegister user

--- src/controllers/registerUser.controller.js
+++ src/controllers/registerUser.controller.js
@@ -64,7 +64,7 @@ export const registerUser = async (req, res) => {
 
 
 export const getRegisterUser = async (req, res) => {
-    const { email } = req.query; 
+    const { email } = req.body; 
 
     if (!email) {
         return res.status(400).json({
